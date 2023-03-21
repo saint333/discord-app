a7916318dc8f6cdf3a653e3b2f1dc6bb6ae65361
David Israel Granados Elias <israeldavi0904@gmail.com>
2023-03-21 00:02:24 -0500

    cambios de nombre

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,7 +3,7 @@ const { isNoAuth, isAuth } = require('../utils/auth');
 const router = Router();
 
 router.get("/",isNoAuth, (req, res) =>{
-    res.render('home')
+    res.render('index')
 })
 
 module.exports = router;
